d5b70b832e91b0d152108849071de2eb0f89479c
Allen <guolong613@gmail.com>
2023-01-02 15:55:19 +0800

    给 gin 添加静态文件路由

--- main.go
+++ main.go
@@ -1,26 +1,50 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
+	"log"
+	"net/http"
 	"os"
 	"os/exec"
 	"os/signal"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
+
+//go:embed frontend/dist/*
+var FS embed.FS
 
 
 func main() {
 	go func() {
 		gin.SetMode(gin.DebugMode)
 		router := gin.Default()
-		router.GET("/", func(c *gin.Context) {
-			c.Writer.Write([]byte("Hello World")) //
-		})
+		staticFiles, _ := fs.Sub(FS, "frontend/dist")
+		router.StaticFS("/static", http.FS(staticFiles))   //所有以static开头的路由，都会自动读取 dist 目录里面的文件
+		router.NoRoute(func(c *gin.Context) {
+			path := c.Request.URL.Path
+			if strings.HasPrefix(path,"/static/"){
+				reader, err := staticFiles.Open("index.html")
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer reader.Close()
+				stat, err := reader.Stat()
+				if err != nil {
+					log.Fatal(err)
+				}
+				c.DataFromReader(http.StatusOK,stat.Size(), "text/html;charset=utf-8", reader, nil)
+			} else {
+				c.Status(http.StatusNotFound)
+			}
+			})
 		router.Run(":8080")
 	}()  // 开一个协程
 
 	chromePath := "D:\\Google\\Chrome\\Application\\chrome.exe"
-	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:8080/")
+	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:8080/static/index.html")
 	cmd.Start()  // 开一个新的进程
 	
 	chSignal := make(chan os.Signal, 1)
