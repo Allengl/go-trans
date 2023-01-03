d12b2d9d36868d2793573ff9de19715cdb3bc800
Allen <guolong613@gmail.com>
2023-01-03 06:26:33 +0000

    重构: 让 main 更合理

--- main.go
+++ main.go
@@ -10,25 +10,30 @@ import (
 
 
 func main() {
-	port := "27149"
 
 	// 启动 gin 服务
-	go func() {
-		server.Run()
-	}()  // 开一个协程
+	go server.Run()
 
-	// 启动 Chrome
-	chromePath := "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:"+port+"/static/index.html")
-	cmd.Start()  // 开一个新的进程
-	
-	// 监听中断信号
-	chSignal := make(chan os.Signal, 1)
-	signal.Notify(chSignal, os.Interrupt)
+	cmd := startBrowser()
 
-	// 等待中断信号
+	chSignal := listenToInterrupt()
 	select {
 	case <-chSignal: // 阻塞等待信号
 	cmd.Process.Kill()
 	}
+}
+
+func startBrowser() *exec.Cmd {
+	port := "27149"
+	chromePath := "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:"+port+"/static/index.html")
+	cmd.Start()  // 开一个新的进程	
+	return cmd
+}
+
+// 监听中断信号
+func listenToInterrupt() chan os.Signal {
+	chSignal := make(chan os.Signal, 1)
+	signal.Notify(chSignal, os.Interrupt)
+	return chSignal
 }
