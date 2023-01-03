9046e4945237b164b531184ca5ac1201e96db138
Allen <guolong613@gmail.com>
2023-01-03 07:57:58 +0000

    Ctrl + C 和关闭按钮都可以完美退出

--- main.go
+++ main.go
@@ -10,27 +10,35 @@ import (
 )
 
 func main() {
-
-	// 启动 gin 服务
-	go server.Run()
-
-	cmd := startBrowser()
+	chChromeDie := make(chan struct{})
+	chBackendDie := make(chan struct{})
 	chSignal := listenToInterrupt()
+	go server.Run()
+	go startBrowser(chChromeDie, chBackendDie)
+	for {
+		select {
+		case <-chSignal: // 阻塞等待信号
 
-	select {
-	case <-chSignal: // 阻塞等待信号
-		cmd.Process.Kill()
+		case <-chChromeDie:
+			os.Exit(0)
+		}
 	}
 }
 
-func startBrowser() *exec.Cmd {
+func startBrowser(chChromeDie chan struct{}, chBackendDie chan struct{}) {
 	chromePath := "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
 	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:"+config.GetPort()+"/static/index.html")
 	cmd.Start() // 开一个新的进程
-	return cmd
+	go func() {
+		<-chBackendDie
+		cmd.Process.Kill()
+	}()
+	go func() {
+		cmd.Wait()
+		chChromeDie <- struct{}{}
+	}()
 }
 
-// 监听中断信号
 func listenToInterrupt() chan os.Signal {
 	chSignal := make(chan os.Signal, 1)
 	signal.Notify(chSignal, os.Interrupt)
