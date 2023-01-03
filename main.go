c6bd71eced61445b8c68ac6cbca20dd74638eca3
Allen <guolong613@gmail.com>
2023-01-03 06:18:49 +0000

    重构: 把启动server的代码放到server.go

--- main.go
+++ main.go
@@ -1,179 +1,34 @@
 package main
 
 import (
-	"embed"
-	"io/fs"
-	"log"
-	"net/http"
 	"os"
 	"os/exec"
 	"os/signal"
-	"strings"
-	"path"
-	"path/filepath"
-	"io/ioutil"
-	"net"
 
-	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
-	"github.com/skip2/go-qrcode"
+	"github.com/Allengl/go-trans/server"
 )
-
-//go:embed frontend/dist/*
-var FS embed.FS
 
 
 func main() {
 	port := "27149"
+
+	// 启动 gin 服务
 	go func() {
-		gin.SetMode(gin.DebugMode)
-		router := gin.Default()
-		staticFiles, _ := fs.Sub(FS, "frontend/dist")
-		router.POST("/api/v1/files", FilesController)
-		router.GET("api/v1/qrcodes", QrcodesController)
-		router.GET("/uploads/:path", UploadsController)
-		router.GET("/api/v1/addresses", AddressesController)
-		router.POST("/api/v1/texts", TextsController)
-		router.StaticFS("/static", http.FS(staticFiles))   //所有以static开头的路由，都会自动读取 dist 目录里面的文件
-		router.NoRoute(func(c *gin.Context) {
-			path := c.Request.URL.Path
-			if strings.HasPrefix(path,"/static/"){
-				reader, err := staticFiles.Open("index.html")
-				if err != nil {
-					log.Fatal(err)
-				}
-				defer reader.Close()
-				stat, err := reader.Stat()
-				if err != nil {
-					log.Fatal(err)
-				}
-				c.DataFromReader(http.StatusOK,stat.Size(), "text/html;charset=utf-8", reader, nil)
-			} else {
-				c.Status(http.StatusNotFound)
-			}
-			})
-		router.Run(":" + port)
+		server.Run()
 	}()  // 开一个协程
 
-	chromePath := "D:\\Google\\Chrome\\Application\\chrome.exe"
+	// 启动 Chrome
+	chromePath := "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
 	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:"+port+"/static/index.html")
 	cmd.Start()  // 开一个新的进程
 	
+	// 监听中断信号
 	chSignal := make(chan os.Signal, 1)
 	signal.Notify(chSignal, os.Interrupt)
 
+	// 等待中断信号
 	select {
 	case <-chSignal: // 阻塞等待信号
 	cmd.Process.Kill()
 	}
-}
-
-func TextsController(c *gin.Context){
-	var json struct {
-		Raw string `json:"raw"`
-	}
-	if err := c.ShouldBindJSON(&json); err != nil { // 读取请求体
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	} else {
-		exe, err := os.Executable() // 获取当前可执行文件的路径
-		if err != nil {
-			log.Fatal(err)
-		}
-		dir := filepath.Dir(exe) // 获取当前可执行文件的目录
-		if err != nil {
-			log.Fatal(err)
-		}
-		filename := uuid.New().String() // 生成一个随机的文件名
-		uploads := filepath.Join(dir, "uploads") // 拼接出 uploads 的绝对路径
-		err = os.MkdirAll(uploads, os.ModePerm) // 创建 uploads 目录
-		if err != nil {
-			log.Fatal(err)
-		}
-		fullpath := path.Join("uploads",filename+".txt") // 拼接出文件的绝对路径（不含目录）
-		err = ioutil.WriteFile(filepath.Join(dir, fullpath), []byte(json.Raw), 0644) // 将 json.Raw 写入文件
-		if err != nil {
-			log.Fatal(err)
-		}
-		c.JSON(http.StatusOK, gin.H{"url":"/" + fullpath}) // 返回文件的绝对路径 （不含 exe 所在目录）
-	}
-}
-
-func FilesController(c *gin.Context){
-	file, err := c.FormFile("raw")
-	if err != nil {
-		log.Fatal(err)
-	}
-	exe, err := os.Executable()
-	if err != nil {
-		log.Fatal(err)
-	}
-	dir := filepath.Dir(exe)
-	if err != nil {
-		log.Fatal(err)
-	}
-	filename := uuid.New().String()
-	uploads := filepath.Join(dir, "uploads")
-	err = os.MkdirAll(uploads, os.ModePerm)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fullpath := path.Join("uploads", filename+filepath.Ext(file.Filename))
-	fileErr := c.SaveUploadedFile(file, filepath.Join(dir, fullpath))
-	if fileErr != nil {
-		log.Fatal(fileErr)
-	}
-	c.JSON(http.StatusOK, gin.H{"url": "/" + fullpath})
-}
-
-func QrcodesController(c *gin.Context) {
-	if content := c.Query("content"); content != "" {
-		png, err := qrcode.Encode(content, qrcode.Medium, 256)
-		if err != nil {
-			log.Fatal(err)
-			} else {
-			c.Data(http.StatusOK, "image/png", png)
-		}
-	} else {
-		c.Status(http.StatusBadRequest)
-	}
-}
-
-
-func GetUploadsDir() (uploads string) {
-	exe, err := os.Executable()
-	if err != nil {
-		log.Fatal(err)
-	}
-	dir := filepath.Dir(exe)
-	uploads = filepath.Join(dir, "uploads")
-	return
-}
-
-
-func UploadsController(c *gin.Context){
-	if path := c.Param("path"); path != "" {
-		target := filepath.Join(GetUploadsDir(), path)
-		c.Header("Content-Descrption", "File Transfer")
-		c.Header("Content-Transfer-Encoding", "binary")
-		c.Header("Content-Disposition", "attachment; filename="+path)
-		c.Header("Content-Type", "application/octet-stream")
-		c.File(target)
-	} else {
-		c.Status(http.StatusNotFound)
-	}
-}
-
-
-
-func AddressesController(c *gin.Context){
-	addrs, _ := net.InterfaceAddrs() // 获取本机所有的 IP 地址
-	var result []string
-	for _, addr := range addrs { // 遍历所有的 IP 地址
-		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil { // 只获取 ipv4 地址
-				result = append(result, ipnet.IP.String()) // 将所有的 IPv4 地址添加到 result 中
-			}
-		}
-	}
-	c.JSON(http.StatusOK, gin.H{"addresses": result}) // 返回所有的 IPv4 地址
 }
