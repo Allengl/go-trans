ca4f1126f251b1d0bb522247fd9427a9193a7ca2
Allen <guolong613@gmail.com>
2023-01-03 05:29:04 +0000

    添加文件上传api

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ func main() {
 		gin.SetMode(gin.DebugMode)
 		router := gin.Default()
 		staticFiles, _ := fs.Sub(FS, "frontend/dist")
+		router.POST("/api/v1/files", FilesController)
 		router.GET("api/v1/qrcodes", QrcodesController)
 		router.GET("/uploads/:path", UploadsController)
 		router.GET("/api/v1/addresses", AddressesController)
@@ -94,6 +95,33 @@ func TextsController(c *gin.Context){
 		}
 		c.JSON(http.StatusOK, gin.H{"url":"/" + fullpath}) // 返回文件的绝对路径 （不含 exe 所在目录）
 	}
+}
+
+func FilesController(c *gin.Context){
+	file, err := c.FormFile("raw")
+	if err != nil {
+		log.Fatal(err)
+	}
+	exe, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dir := filepath.Dir(exe)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filename := uuid.New().String()
+	uploads := filepath.Join(dir, "uploads")
+	err = os.MkdirAll(uploads, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fullpath := path.Join("uploads", filename+filepath.Ext(file.Filename))
+	fileErr := c.SaveUploadedFile(file, filepath.Join(dir, fullpath))
+	if fileErr != nil {
+		log.Fatal(fileErr)
+	}
+	c.JSON(http.StatusOK, gin.H{"url": "/" + fullpath})
 }
 
 func QrcodesController(c *gin.Context) {
